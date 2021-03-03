9a2e75d1ddf3b6b8b78da2fd4b27ddf4c1ebaff1
贝贝 <1368698567@qq.com>
2021-03-03 09:52:10 +0800

    test

--- src/HiCloundJava.java
+++ src/HiCloundJava.java
@@ -1,6 +1,6 @@
 public class HiCloundJava {
         public static void main(String args[]){
-            System.out.println("HiGit!2021");
+            System.out.println("HiGit!2021!");
         }
     }
 
