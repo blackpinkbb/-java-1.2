c2ea09da0fba2ccbfbff1a0449b83c68c15c401a
blackpinkbb <1368698567@qq.com>
2021-03-04 14:31:13 +0800

    test

--- src/HiCloundJava.java
+++ src/HiCloundJava.java
@@ -1,6 +1,7 @@
 public class HiCloundJava {
         public static void main(String args[]){
             System.out.println("HiGit!2021.");
+            System.out.println("背心第二次提交的");
         }
     }
 
